56a61b7b2605871d425a54d26752287ec92409c8
Glenmond <glenmond@outlook.com>
2021-10-29 03:19:29 +0800

    Change data folder structure

--- analytics/data/import_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from fredapi import Fred
-fred_api = '18fb1a5955cab2aae08b90a2ff0f6e42'
-fred = Fred(api_key=fred_api, response_type = 'df')
-
-def fetch_data(type:str):
-    pass
-    # TODO: handle our data i/o, fetch data using API or what
