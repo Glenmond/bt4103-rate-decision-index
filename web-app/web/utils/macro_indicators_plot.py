b3d12aed21e555ec68031777ea62f015702139b1
ericajanetem <ariecarh@gmail.com>
2021-11-09 21:54:45 +0800

    Remove web-app folder

--- web-app/web/utils/macro_indicators_plot.py
+++
@@ -1,3 +0,0 @@
-# TODO: write a function for getting data from a pickle file, and if the pickle file is not there, get from API (in utils.py)
-# TODO: Plot the graph using the data
-# TODO: learn the slider thing and how it can be integrated here
