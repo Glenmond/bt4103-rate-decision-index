88850e216feba1bc0233da14433464937e0edf65
angjamais <ajamais3105@gmail.com>
2021-11-09 18:56:48 +0800

    updated comments for gauge plot

--- web-app/web/routes.py
+++ web-app/web/routes.py
@@ -26,13 +26,10 @@ main = Main()
 #market
 #market_data = utils.load_market_data()
 statement_df, mins_df, news_df = utils.load_market_data()
-#market_data_cleaned = utils.clean_market(market_data)
 market_data_cleaned = utils.import_modify_pickle_ms_main(statement_df, mins_df, news_df)
 
-market_ngram_statement, market_ngram_min = utils.load_ngram_market_data(year=2004)
-#market_ngram_min, market_ngram_statement, market_ngram_news = utils.load_ngram_market_data()
-#market_ngram_min_cleaned = utils.clean_ngram_data(market_ngram_min)
-#market_ngram_statement_cleaned = utils.clean_ngram_data(market_ngram_statement)
+market_ngram_statement, market_ngram_min, market_ngram_news = utils.load_ngram_market_data(year=2004)
+
 
 #macro
 gdp_data, employment_data, inflation_data = utils.load_macro_data()
@@ -49,65 +46,12 @@ fff_data_cleaned = utils.clean_fff(fff_data)
 #home
 home_data = utils.load_home_data()
 
+###gmond add data loader here and cleaner if needed here 
+
+
 #to change the data for home and start page
-@app.route("/")
+@app.route("/",  methods=['GET', 'POST'])
 def plot_main_dashboard():
-    #ploting
-    #market
-    plot_market_senti_main = home_plot.plot_market(market_data, date='2004-09-30')
-    plot_market_average = home_plot.plot_market_average(market_data_cleaned, date='2004-09-30')
-    
-    
-    #macro
-    plot_gdp_index = home_plot.plot_gdp_index(home_data)
-    #plot_macro_average = home_plot.plot_market_average2(market_data)
-    macro_ts_plot = home_plot.plot_fed_rates_ts(macro_ts)
-    print(macro_maindashboard_data.head())
-    macro_maindashboard_plot = home_plot.plot_macro_maindashboard(macro_maindashboard_data, date='2004-09-30')
-    macro_pie_chart = home_plot.plot_contributions_pie(macro_maindashboard_data, date='2004-09-30')
-    #fff
-    plot_fff = home_plot.plot_fff(fff_data)
-    #plot_fff_average = home_plot.plot_market_average3(market_data)
-
-    # gauge
-    plot_gauge = home_plot.plot_gauge()
-
-    context = {
-            "plot_market_senti_main": plot_market_senti_main,
-            "plot_market_average": plot_market_average,
-            #"plot_macro_average": plot_macro_average,
-            #"plot_fff_average": plot_fff_average,
-            'plot_gdp_index': plot_gdp_index, 
-            'plot_fff': plot_fff,
-            "plot_gauge": plot_gauge,
-            'macro_ts_plot':macro_ts_plot,
-            'macro_maindashboard_plot':macro_maindashboard_plot,
-            'macro_pie_chart':macro_pie_chart}
-    return render_template('home.html', context=context)
-
-@app.route("/upload")
-def upload_file():
-    return render_template('model-run.html')
-
-@app.route("/runfff", methods = ['GET', 'POST'])
-def run_fff_model():
-    main.run_main()
-    return render_template('model-run.html')
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_files():
-   if request.method == 'POST':
-      f = request.files['file']
-      path = os.path.join(uploads_dir, secure_filename(f.filename))
-      f.save(path)
-      return 'file uploaded successfully'
-
-class PostForm(FlaskForm):
-    date = StringField('date', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-@app.route("/home",  methods=['GET', 'POST'])
-def plot_home():
     form = PostForm()
     
     if request.method == 'POST':
@@ -137,8 +81,6 @@ def plot_home():
         context = {
                 "plot_market_senti_main": plot_market_senti_main,
                 "plot_market_average": plot_market_average,
-                #"plot_macro_average": plot_macro_average,
-                #"plot_fff_average": plot_fff_average,
                 'plot_gdp_index': plot_gdp_index, 
                 'plot_fff': plot_fff,
                 "plot_gauge": plot_gauge,
@@ -171,8 +113,102 @@ def plot_home():
         context = {
                 "plot_market_senti_main": plot_market_senti_main,
                 "plot_market_average": plot_market_average,
-                #"plot_macro_average": plot_macro_average,
-                #"plot_fff_average": plot_fff_average,
+                'plot_gdp_index': plot_gdp_index, 
+                'plot_fff': plot_fff,
+                "plot_gauge": plot_gauge,
+                'macro_ts_plot':macro_ts_plot,
+                'macro_maindashboard_plot':macro_maindashboard_plot,
+                'macro_pie_chart':macro_pie_chart}
+        return render_template('home.html', context=context, form=form)
+
+@app.route("/upload")
+def upload_file():
+    return render_template('model-run.html')
+
+@app.route("/runfff", methods = ['GET', 'POST'])
+def run_fff_model():
+    main.run_main()
+    return render_template('model-run.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_files():
+   if request.method == 'POST':
+      f = request.files['file']
+      path = os.path.join(uploads_dir, secure_filename(f.filename))
+      f.save(path)
+      return 'file uploaded successfully'
+
+class PostForm(FlaskForm):
+    date = StringField('date', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route("/home",  methods=['GET', 'POST'])
+def plot_home():
+    form = PostForm()
+    
+    if request.method == 'POST':
+        date = request.form['date-mm']
+        #print(date)
+        #date = '2004-09-30'
+        #print(date)
+        
+        #ploting
+        
+        #market
+        plot_market_senti_main = home_plot.plot_market(market_data_cleaned, date)
+        plot_market_average = home_plot.plot_market_average(market_data_cleaned, date)
+        
+        
+        #macro
+        plot_gdp_index = home_plot.plot_gdp_index(home_data)
+        #plot_macro_average = home_plot.plot_market_average2(market_data)
+        macro_ts_plot = home_plot.plot_fed_rates_ts(macro_ts)
+        macro_maindashboard_plot = home_plot.plot_macro_maindashboard(macro_maindashboard_data, date)
+        macro_pie_chart = home_plot.plot_contributions_pie(macro_maindashboard_data, date)
+        #fff
+        plot_fff = home_plot.plot_fff(fff_data)
+        #plot_fff_average = home_plot.plot_market_average3(market_data)
+
+        # gauge
+        ### gmond update data source here 
+        plot_gauge = home_plot.plot_gauge(market_data_cleaned, date)
+
+        context = {
+                "plot_market_senti_main": plot_market_senti_main,
+                "plot_market_average": plot_market_average,
+                'plot_gdp_index': plot_gdp_index, 
+                'plot_fff': plot_fff,
+                "plot_gauge": plot_gauge,
+                'macro_ts_plot':macro_ts_plot,
+                'macro_maindashboard_plot':macro_maindashboard_plot,
+                'macro_pie_chart':macro_pie_chart}
+        return render_template('home.html', context=context, form=form)
+        
+    else:
+        #ploting
+        #market
+        plot_market_senti_main = home_plot.plot_market(market_data_cleaned, date='2004-09-30')
+        plot_market_average = home_plot.plot_market_average(market_data_cleaned, date='2004-09-30')
+        
+        
+        #macro
+        plot_gdp_index = home_plot.plot_gdp_index(home_data)
+        #plot_macro_average = home_plot.plot_market_average2(market_data)
+        macro_ts_plot = home_plot.plot_fed_rates_ts(macro_ts)
+        print(macro_maindashboard_data.head())
+        macro_maindashboard_plot = home_plot.plot_macro_maindashboard(macro_maindashboard_data, date='2004-09-30')
+        macro_pie_chart = home_plot.plot_contributions_pie(macro_maindashboard_data, date='2004-09-30')
+        #fff
+        plot_fff = home_plot.plot_fff(fff_data)
+        #plot_fff_average = home_plot.plot_market_average3(market_data)
+
+        # gauge
+        ### gmond update data source here 
+        plot_gauge = home_plot.plot_gauge(market_data_cleaned,  date='2004-09-30')
+
+        context = {
+                "plot_market_senti_main": plot_market_senti_main,
+                "plot_market_average": plot_market_average,
                 'plot_gdp_index': plot_gdp_index, 
                 'plot_fff': plot_fff,
                 "plot_gauge": plot_gauge,
@@ -205,9 +241,7 @@ def plot_market_consensus():
                'ngram_statement':ngram_statement}
     return render_template('market-consensus.html', context=context)
 
-
-
-    
+   
 @app.route("/macroeconomic-indicators")
 def plot_macroeconomic_indicators():
     #ploting
