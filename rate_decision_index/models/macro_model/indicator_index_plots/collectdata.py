cbba04aec38762f550bf5540bbcd5d1b1b298edb
angjamais <ajamais3105@gmail.com>
2021-12-07 09:25:07 +0800

    Update collectdata.py

--- rate_decision_index/models/macro_model/indicator_index_plots/collectdata.py
+++ rate_decision_index/models/macro_model/indicator_index_plots/collectdata.py
@@ -6,26 +6,6 @@ from sklearn.preprocessing import MinMaxScaler
 
 quandl.ApiConfig.api_key = "kVFwRskyFgKCs3HURnYV"
 
-
-
-indicators_gdp = ['GDP', 'PCE', 'GPDI', 'GCE', 'NETEXP']
-
-def get_data(indicators, start, end):
-    df = pd.DataFrame()
-    scaler = MinMaxScaler()
-    for indicator in indicators:
-        name = "FRED/" + indicator
-        mydata = quandl.get(name, start_date=start, end_date=end, collapse='quarterly', transform = 'none')
-        datecol=mydata.index
-        mydata.index = pd.to_datetime(mydata.index).to_period('Q')
-        mydata.rename(columns={'Value': indicator }, inplace=True)
-        mydata[indicator] = pd.to_numeric(mydata[indicator])
-        df = pd.concat([df, mydata], axis='columns')
-        #print(indicator)
-    df = df.fillna(method='ffill')
-    df['Date']=datecol
-    return df
-
 def get_data2(indicators, start, end):
     df = pd.DataFrame()
     scaler = MinMaxScaler()
@@ -39,7 +19,6 @@ def get_data2(indicators, start, end):
         df = pd.concat([df, mydata], axis='columns')
         #print(indicator)
     df = df.fillna(method='ffill')
-    df['Date']=datecol
     return df
 
 def collect_indicator_data(path="../../data/macroeconomic_indicators_data/"):  
@@ -51,7 +30,6 @@ def collect_indicator_data(path="../../data/macroeconomic_indicators_data/"):
     dfEI = get_data2(indicators=indicators_employment, start='2004-01-01', end='')
     dfEI.to_csv(f"{path}/macro_employment_data.csv", index=True)
 
-    indicators_inflation = ['MEDCPIM158SFRBCLE','CPIAUCSL','CPIFABSL', 'CPIAPPSL', 'CPIMEDSL', 'CPIHOSSL', 'CPITRNSL', 'CPIEDUSL', 'CPIRECSL', 'CPIOGSSL']
     df_inflation = get_data2(indicators=indicators_inflation, start='2004-01-01', end='')
     df_inflation.to_csv(f"{path}/macro_inflation_data.csv", index=True)
 
