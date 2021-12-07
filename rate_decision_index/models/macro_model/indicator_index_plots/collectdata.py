69a94d7f84544468f4c75054c83dd9d1ae3b97f5
angjamais <ajamais3105@gmail.com>
2021-12-07 11:51:08 +0800

    updated collectdata.py

--- rate_decision_index/models/macro_model/indicator_index_plots/collectdata.py
+++ rate_decision_index/models/macro_model/indicator_index_plots/collectdata.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 quandl.ApiConfig.api_key = "kVFwRskyFgKCs3HURnYV"
 
+
 def get_data2(indicators, start, end):
     df = pd.DataFrame()
     scaler = MinMaxScaler()
@@ -30,6 +31,7 @@ def collect_indicator_data(path="../../data/macroeconomic_indicators_data/"):
     dfEI = get_data2(indicators=indicators_employment, start='2004-01-01', end='')
     dfEI.to_csv(f"{path}/macro_employment_data.csv", index=True)
 
+    indicators_inflation = ['MEDCPIM158SFRBCLE','CPIAUCSL','CPIFABSL', 'CPIAPPSL', 'CPIMEDSL', 'CPIHOSSL', 'CPITRNSL', 'CPIEDUSL', 'CPIRECSL', 'CPIOGSSL']
     df_inflation = get_data2(indicators=indicators_inflation, start='2004-01-01', end='')
     df_inflation.to_csv(f"{path}/macro_inflation_data.csv", index=True)
 
